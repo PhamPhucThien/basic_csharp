f925d006e86fdf3c0ef3a920296efe1e6e9fcea2
THIEN <sherrin1st@gmail.com>
2023-11-17 20:31:15 +0700

    push all code to github

---
+++ basic_csharp/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_csharp.Models
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+
+        public Guid AccountId { get; set; }
+        public string CartRecord { get; set; }
+    }
+}
